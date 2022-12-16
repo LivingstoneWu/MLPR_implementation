dbe5ff26895dd1109c06bc49fb7f2c8ebeb5fdf4
Livingstone Wu <livingstone.wu00@gmail.com>
2022-12-16 03:20:08 +0000

    Generate Phi

---
+++ models.py
@@ -0,0 +1,4 @@
+"""
+Basic Linear regression model
+"""
+def lin_reg(X, yy, )
---
+++ preprocessing.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+"""Generate preprocessed input matrix Phi. 
+
+Phi=[phi(xx_i)]
+Generate preprocessed input matrix Phi, with each row containing the result of application of basis functions on the
+corresponding xx sample. Optional with regularization terms.
+
+    Arguments
+    ----------
+    X: ndarray
+        original feature matrix
+    basis_funcs: list
+        list of vectorized basis functions to be applied on the feature matrix.
+    reg_factor: float, optional
+        L2 regularization penalty factor lambda. 
+    
+    Returns
+    ----------
+    ndarray
+        a ndarray of the transformed feature matrix Phi.
+"""
+
+
+def gen_phi(X, basis_funcs, reg_factor=0):
+    res = []
+    for row in X:
+        row_res = []
+        for basis_f in basis_funcs:
+            row_res.append(basis_f(row))
+        res.append(row_res)
+    res = np.array(res)
+    if reg_factor != 0:
+        res = np.concatenate((res, np.identity(res.shape[1])), axis=0)
+    return res
+
