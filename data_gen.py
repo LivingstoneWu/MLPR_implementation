ef262b8f48e1efbd24c78e8739c11edc14172cf6
LivingstoneWu <livingstone.wu00@gmail.com>
2022-12-18 19:42:53 +0000

    update linreg

--- data_gen.py
+++ data_gen.py
@@ -1,1 +1,22 @@
-def lin_with_gaussian(w_mean, w_var, b_mean, b_var, num_points):
+import numpy as np
+
+def add_gaussian(yy, noise, use_seed=False, random_seed=0):
+    if use_seed:
+        rs=np.random.RandomState(seed=random_seed)
+    else:
+        rs=np.random.RandomState()
+    return yy+rs.normal(0, noise, yy.shape[0])
+
+
+def lin1d_with_gaussian(w_mean=0, w_var=2, b_mean=0, b_var=2, noise=0.5, num_points=20, interval=(-10, 10), use_seed=False, random_seed=0):
+    if use_seed:
+        rs=np.random.RandomState(seed=random_seed)
+    else:
+        rs=np.random.RandomState()
+    xx=rs.rand(num_points)*(interval[1]-interval[0])+interval[0]
+    w=rs.normal(w_mean, w_var)
+    b=rs.normal(b_mean, b_var)
+    yy=xx @ w + b
+    return xx, add_gaussian(yy, noise, use_seed, random_seed)
+
+
--- functions.py
+++ functions.py
@@ -1,7 +1,17 @@
 import math
+import numpy as np
 
 def RBF(xx, cc, h):
     return math.exp(-(xx-cc)**2/h**2)
 
 def sigmoid(a):
     return 1/(1+math.exp(-a))
+
+def list_RBFs(ccs, hs):
+    if len(ccs)!=len(hs):
+        raise RuntimeError("RBF list: The list of center locations and hs are expected to have same lengths.")
+    RBFs=[]
+    for i in range(len(ccs)):
+        RBFs.append(lambda xx:RBF(xx, ccs[i], hs)[i])
+    return RBFs
+
